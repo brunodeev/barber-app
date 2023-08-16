a5ad4e91758bc07884591f8f8c7ef6eb6d073dce
Bruno César <bcgmeireles@gmail.com>
2023-08-16 20:15:54 -0300

    feat: upgrade login page design

--- src/pages/entry/LoginScreen.js
+++ src/pages/entry/LoginScreen.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { Image, StyleSheet, Text, SafeAreaView, TextInput, View, TouchableOpacity, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { Image, StyleSheet, Text, SafeAreaView, TextInput, View, TouchableOpacity, Platform, KeyboardAvoidingView, ScrollView } from 'react-native';
 import barber from '../../assets/barber.png';
 
 const LoginScreen = () => {
     return (
-        <KeyboardAvoidingView style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image source={barber} style={styles.image}/>
-            </View>
-            <View style={styles.inputs}>
-                <TextInput placeholder='Email' onChange={() => {}} autoCorrect={false} keyboardType='email-address' style={styles.input}/>
-                <TextInput placeholder='Senha' onChange={() => {}} secureTextEntry={true} style={styles.input}/>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={{ color: '#FFF', fontSize: 17 }}>Entrar</Text>
-                </TouchableOpacity>
-            </View>
+        <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <SafeAreaView style={styles.container}>
+                <ScrollView contentContainerStyle={styles.scrollView}>
+                    <View style={styles.container}>
+                        <View style={styles.imageContainer}>
+                            <Image source={barber} style={styles.image}/>
+                        </View>
+                        <View style={styles.inputs}>
+                            <TextInput placeholder='Email' onChange={() => {}} autoCorrect={false} keyboardType='email-address' style={styles.input}/>
+                            <TextInput placeholder='Senha' onChange={() => {}} secureTextEntry={true} style={styles.input}/>
+                            <TouchableOpacity style={styles.forget}>
+                                <Text style={{color: '#b1b1b1', textAlign: 'right'}}>Esqueci a senha</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={{ color: '#FFF', fontSize: 17 }}>Entrar</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style={styles.haveAccount}>
+                        <Text style={{color: '#b1b1b1'}}>Não tem uma conta? </Text>
+                        <TouchableOpacity>
+                            <Text>Crie uma</Text>
+                        </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         </KeyboardAvoidingView>
+        
     );
 };
 
@@ -29,37 +45,52 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF',
     },
     imageContainer: {
-        flex: 1,
-        justifyContent: 'center',
+        marginBottom: 50,
+        height: 200,
+        width: 350,
     },
-    imageContainer: {
-        height: 20,
-        width: 20,
-        alignContent: 'center',
+    image: {
+        height: undefined,
+        width: undefined,
+        flex: 1,
+    },
+    scrollView: {
+        flex: 1,
+        marginTop: 20,
+        width: '100%',
         justifyContent: 'center',
     },
     input: {
         backgroundColor: '#e9e9e9',
-        width: '90%',
+        width: '95%',
         fontSize: 18,
         borderRadius: 15,
-        height: 55,
+        height: 45,
         paddingHorizontal: 12,
-        marginBottom: 15,
+        marginBottom: 12,
         shadowColor: '#000',
-        shadowOffset: 0,
-        shadowOpacity: .3,
+        shadowOffset: {
+            width: 3,
+            height: 3,
+        },
+        shadowOpacity: .2,
         shadowRadius: 2.5,
     },
     inputs: {
-        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginVertical: 30,
-        width: '90%',
+        width: '95%',
+    },
+    forget: {
+        paddingRight: 14,
+        width: '100%',
+    },
+    haveAccount: {
+        flexDirection: 'row',
+        justifyContent: 'center',
     },
     button: {
-        width: '90%',
+        width: '95%',
         marginVertical: 20,
         backgroundColor: '#283130',
         height: 50,
