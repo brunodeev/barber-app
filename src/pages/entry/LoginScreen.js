d6329d1302ecef7cbf5d073e31c78d83bb72f13a
Bruno César <bcgmeireles@gmail.com>
2023-08-16 15:20:36 -0300

    feat: try to create a responsive input

--- src/pages/entry/LoginScreen.js
+++ src/pages/entry/LoginScreen.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Image, StyleSheet, Text, SafeAreaView, TextInput, View, TouchableOpacity, ScrollView } from 'react-native';
+import { Image, StyleSheet, Text, SafeAreaView, TextInput, View, TouchableOpacity, ScrollView, KeyboardAvoidingView } from 'react-native';
 import barber from '../../assets/barber.png';
 
 const LoginScreen = () => {
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Entrar</Text>
-            <ScrollView>
+        <KeyboardAvoidingView style={styles.container}>
+            <View style={styles.imageContainer}>
                 <Image source={barber} style={styles.image}/>
-                <View style={styles.inputs}>
-                    <TextInput placeholder='Nome' onChange={() => {}} style={styles.input}/>
-                    <TextInput placeholder='Senha' onChange={() => {}} style={styles.input}/>
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={{ color: '#FFF', fontSize: 17 }}>Entrar</Text>
-                    </TouchableOpacity>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+            </View>
+            <View style={styles.inputs}>
+                <TextInput placeholder='Email' onChange={() => {}} autoCorrect={false} keyboardType='email-address' style={styles.input}/>
+                <TextInput placeholder='Senha' onChange={() => {}} secureTextEntry={true} style={styles.input}/>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={{ color: '#FFF', fontSize: 17 }}>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     );
 };
 
@@ -25,25 +24,26 @@ export default LoginScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor: '#FFF',
     },
-    title: {
-        fontSize: 30,
-        height: 40,
-        textAlign: 'center',
-    },
-    image: {
-        marginTop: 50,
+    imageContainer: {
+        flex: 1,
         justifyContent: 'center',
-        width: '100%',
-        height: 250,
+    },
+    imageContainer: {
+        height: 20,
+        width: 20,
+        alignContent: 'center',
+        justifyContent: 'center',
     },
     input: {
         backgroundColor: '#e9e9e9',
+        width: '90%',
         fontSize: 18,
         borderRadius: 15,
         height: 55,
-        marginHorizontal: 14,
         paddingHorizontal: 12,
         marginBottom: 15,
         shadowColor: '#000',
@@ -52,13 +52,17 @@ const styles = StyleSheet.create({
         shadowRadius: 2.5,
     },
     inputs: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
         marginVertical: 30,
+        width: '90%',
     },
     button: {
+        width: '90%',
         marginVertical: 20,
         backgroundColor: '#283130',
         height: 50,
-        marginHorizontal: 12,
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 15,
